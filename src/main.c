d450e968a980e0804279b269a38d83de72f44d24
JM-Pilot <gabmanlapaz67@gmail.com>
2026-07-15 19:07:54 +0800

    Changed Delay and removed misleading info

--- src/main.c
+++ src/main.c
@@ -6,12 +6,9 @@
  */
 #include <Arduino.h>
 
-/* 0 and 1 is tx and rx they are used to talk to the computer, 
- * instead we will use 2, 3 and 4 to be safe
- */
-#define PIN0 2
-#define PIN1 3
-#define PIN2 4
+#define PIN0 1
+#define PIN1 2
+#define PIN2 3
 
 /* change this where the wire is */
 #define LED_GREEN PIN0
@@ -19,8 +16,8 @@
 #define LED_YELLOW PIN2
 
 /* change this if you want to change delays */
-#define GREEN_DELAY 5000 // 5 sec
-#define YELLOW_DELAY 2000 // 2 sec
+#define GREEN_DELAY 7000 // 5 sec
+#define YELLOW_DELAY 3000 // 2 sec
 #define RED_DELAY 7000 // 7 sec
 
 /* initialize leds, and other sensors */
@@ -39,10 +36,12 @@ void loop()
 	digitalWrite(LED_RED, LOW);
 	digitalWrite(LED_GREEN, HIGH);
 	delay(GREEN_DELAY);
+
 	/* Show yellow, disable green */
 	digitalWrite(LED_GREEN, LOW);
 	digitalWrite(LED_YELLOW, HIGH);
 	delay(YELLOW_DELAY);
+
 	/* Show Red, disable yellow */
 	digitalWrite(LED_YELLOW, LOW);
 	digitalWrite(LED_RED, HIGH);
